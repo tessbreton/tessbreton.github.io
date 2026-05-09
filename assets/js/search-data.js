566187feb48ddab81a768ffd8b7fda93021b6025
tessbreton <61019583+tessbreton@users.noreply.github.com>
2026-05-09 11:11:47 +0000

    Deploying to gh-pages from @ tessbreton/tessbreton.github.io@c414785a8be2dbb157a391c54d2057c581ece4ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "Teaching",
+          title: "teaching",
           description: "",
           section: "Navigation",
           handler: () => {
